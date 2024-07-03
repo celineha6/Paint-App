a6e80bc45677c4517b5f297c0377bb31da531f73
Celine Ha <51255754+celineha6@users.noreply.github.com>
2024-07-03 00:05:43 -0700

    Update StatusPanel.java

--- StatusPanel.java
+++ StatusPanel.java
@@ -1,24 +1,74 @@
 package javiergs.gui.paint.gamma;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Stack;
 
-/**
- * StatusPanel creates a panel with two buttons: Undo and Erase.
- * This version includes an ActionListener.
- *
- * @author javiergs
- * @version 2.0
- */
 public class StatusPanel extends JPanel {
-	
-	public StatusPanel() {
-		JButton button1 = new JButton("Undo");
-		JButton button2 = new JButton("Erase");
-		add(button1);
-		add(button2);
-		ActionNanny actionNanny = new ActionNanny(); // new line compared to version 1.0
-		button1.addActionListener(actionNanny); // new line compared to version 1.0
-		button2.addActionListener(actionNanny); // new line compared to version 1.0
-	}
-	
+
+    private Stack<Action> undoStack = new Stack<>();
+    private Stack<Action> redoStack = new Stack<>();
+
+    public StatusPanel() {
+        JButton buttonUndo = new JButton("Undo");
+        JButton buttonErase = new JButton("Erase");
+        add(buttonUndo);
+        add(buttonErase);
+
+        ActionNanny actionNanny = new ActionNanny();
+        buttonUndo.addActionListener(actionNanny);
+        buttonErase.addActionListener(actionNanny);
+    }
+
+    private void performAction(Action action) {
+        action.execute();
+        undoStack.push(action);
+        redoStack.clear();
+    }
+
+    private void undoAction() {
+        if (!undoStack.isEmpty()) {
+            Action action = undoStack.pop();
+            action.undo();
+            redoStack.push(action);
+        }
+    }
+
+    private void redoAction() {
+        if (!redoStack.isEmpty()) {
+            Action action = redoStack.pop();
+            action.execute();
+            undoStack.push(action);
+        }
+    }
+
+    private interface Action {
+        void execute();
+        void undo();
+    }
+
+    private class EraseAction implements Action {
+        @Override
+        public void execute() {
+            System.out.println("Erase executed");
+        }
+
+        @Override
+        public void undo() {
+            System.out.println("Erase undone");
+        }
+    }
+
+    private class ActionNanny implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+            if (command.equals("Undo")) {
+                undoAction();
+            } else if (command.equals("Erase")) {
+                performAction(new EraseAction());
+            }
+        }
+    }
 }
